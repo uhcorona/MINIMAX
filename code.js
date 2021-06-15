a647feec91f54d110c26f8ff1b2bc82e37606186
René Corona <3114327190409@ingenieria.usac.edu.gt>
2021-06-15 11:37:14 -0600

    Commit 7

--- code.js
+++ code.js
@@ -1,3 +1,1 @@
-const urlSearchParams = new URLSearchParams(window.location.search);
-const params = Object.fromEntries(urlSearchParams.entries());
-console.log(params);
+document.body.innerHTML = Number(24);
--- test.php
+++
@@ -1,6 +0,0 @@
-header("Content-Type:text/plain");
-
-ob_start();
-echo hola;
-ob_flush();
-ob_end_clean();
